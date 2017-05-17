885ffb206f920a173fee7f617055a037aced6d6e
Michael Szul <michael@szul.us>
2017-05-17 08:50:34 -0400

    Initial project shell

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # edUi Conference 2017 Presentation
 
-A bot example using cognitive services for my edUi Conference talk.
+This repo houses the coding examples and demos for my [edUi Conference](http://eduiconf.org) (2017) talk on [bots](http://eduiconf.org/sessions/building-bots-for-the-conversation-ui/).
+
+This particular repository is a sister repository to an [example bot demo](https://github.com/szul/conf-edui2017-examplebot). That repo's code is designed to showcase many of the dialog and UI functionality that Microsoft's Bot Builder offers. This repo is designed to showcase the available cognitive service integration.
+
+### How to install
+
+From a shell:
+
+```
+git clone https://github.com/szul/conf-edui2017-cogbot.git
+cd conf-edui2017-cogbot
+npm install
+```
---
+++ lib/app.ts
@@ -0,0 +1,28 @@
+import * as restify from "restify";
+import * as builder from "botbuilder";
+
+function startServer(): void {
+    var server = restify.createServer();
+    server.listen(process.env.port || process.env.PORT || 3978, () => {
+        console.log("%s listening to %s", server.name, server.url);
+        startBot(server);
+    });
+}
+
+function startBot(server: restify.Server): void {
+    var conn = new builder.ChatConnector({
+        appId: process.env.MICROSOFT_APP_ID,
+        appPassword: process.env.MICROSOFT_APP_PASSWORD
+    });
+    var bot = new builder.UniversalBot(conn, (sess) => {
+        sess.send("Hello. Welcome to the edUi bot. You can learn about the conference schedule and speakers here.");
+    });
+    /*
+     * Add Luis as a recognizer.
+     * Get the Luis model URL from the environment.
+     */
+    bot.recognizer(new builder.LuisRecognizer(process.env.LUIS_MODEL_URL));
+    server.post("/api/messages", conn.listen());
+}
+
+startServer();
